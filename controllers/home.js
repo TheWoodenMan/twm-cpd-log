3ee3fa72df8020a09b8e9c7434d55684b59ff07c
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-09-16 22:51:44 +0100

    fixed create entry
    -create entry fixed
    -placeholders for privacy policy put in

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,17 @@
 module.exports = {
-    getIndex: (req,res)=>{
-        res.render('index.ejs')
-    }
-}
+	getIndex: (req, res) => {
+		res.render("index.ejs");
+	},
+};
+
+exports.getTos = (req, res) => {
+	res.render("tos", {
+		title: "Tos",
+	});
+};
+
+exports.getPrivacy = (req, res) => {
+	res.render("privacy", {
+		title: "Privacy",
+	});
+};
