44b0bb8008df45491d6937b5cdbe05dcd34f6e56
Andy Wood <andynwood@gmail.com>
2024-01-27 11:13:52 +0000

    deprecation warnings resolved

--- controllers/entries.js
+++ controllers/entries.js
@@ -1,69 +1,67 @@
-const Entry = require("../models/Entry");
+const Entry = require('../models/Entry')
 
 module.exports = {
-	getEntries: async (req, res) => {
-		try {
-			const entryItems = await Entry.find({ userId: req.user.id }).sort({
-				"date": -1
-			});
-			res.render("entries.ejs", {
-				entries: entryItems,
-				user: req.user
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	},
-	searchDates: async (req, res) => {
-		try {
-			console.log(req.body.startDate);
-			console.log(req.body.endDate);
-			const entryItems = await Entry.find({
-				userId: req.user.id,
-				date: {
-					$gte: new Date(req.body.startDate),
-					$lt: new Date(req.body.endDate)
-				}
-			}).sort({
-				"date": -1
-			});
-			res.render("entries.ejs", {
-				entries: entryItems,
-				user: req.user
-			});
-		} catch (err) {
-			console.log(err);
-		}
-	},
+  getEntries: async (req, res) => {
+    try {
+      const entryItems = await Entry.find({ userId: req.user.id }).sort({
+        date: -1
+      })
+      res.render('entries.ejs', {
+        entries: entryItems,
+        user: req.user
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  searchDates: async (req, res) => {
+    try {
+      console.log(req.body.startDate)
+      console.log(req.body.endDate)
+      const entryItems = await Entry.find({
+        userId: req.user.id,
+        date: {
+          $gte: new Date(req.body.startDate),
+          $lt: new Date(req.body.endDate)
+        }
+      }).sort({
+        date: -1
+      })
+      res.render('entries.ejs', {
+        entries: entryItems,
+        user: req.user
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  },
 
-	createEntry: async (req, res) => {
-		console.log(req.body);
-		try {
-			await Entry.create({
-				summary: req.body.summary,
-				date: new Date(req.body.date),
-				userId: req.user.id,
-				cause: req.body.cause,
-				action: req.body.action,
-				result: req.body.result,
-				learned: req.body.learned
-			});
-			console.log(`Entry ${req.body.summary} has been added!`);
-			res.redirect("/entries");
-		} catch (err) {
-			console.log(err);
-		}
-	},
-	deleteEntry: async (req, res) => {
-		try {
-			// Find post by id
-			let entry = await Entry.findById({ _id: req.params.id });
-			// Delete post from db
-			await Entry.remove({ _id: req.params.id });
-			console.log("Deleted Entry");
-			res.redirect("/entries");
-		} catch (err) {
-			res.redirect("/entries");
-		}
-	}
-};
+  createEntry: async (req, res) => {
+    //  console.log(req.body) debug only
+    try {
+      await Entry.create({
+        summary: req.body.summary,
+        date: new Date(req.body.date),
+        userId: req.user.id,
+        cause: req.body.cause,
+        action: req.body.action,
+        result: req.body.result,
+        learned: req.body.learned
+      })
+      console.log(`Entry ${req.body.summary} has been added!`)
+      res.redirect('/entries')
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  deleteEntry: async (req, res) => {
+    try {
+      // Delete post from db
+      await Entry.deleteOne({ _id: req.params.id })
+      console.log(`Deleted Entry ${req.params.id}`)
+      res.redirect('/entries')
+    } catch (err) {
+      res.redirect('/entries')
+    }
+  }
+}
--- server.js
+++ server.js
@@ -1,60 +1,61 @@
 // declare node.js module variables.
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const methodOverride = require("method-override");
-const MongoStore = require("connect-mongo");
-const flash = require("express-flash");
-const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const entryRoutes = require("./routes/entries");
-require("./config/passport")(passport);
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const MongoStore = require('connect-mongo')
+const flash = require('express-flash')
+const logger = require('morgan')
+const connectDB = require('./config/database')
+const mainRoutes = require('./routes/main')
+const entryRoutes = require('./routes/entries')
+require('./config/passport')(passport)
 
 // enable dotenv for secrets
-require("dotenv").config({ path: "./config/.env" });
-const PORT = process.env.PORT;
+require('dotenv').config({ path: './config/.env' })
+const PORT = process.env.PORT
 
 // mongo variables
 let db,
-	dbString = process.env.DB_STRING;
-dbName = "entries";
+  dbString = process.env.DB_STRING
+dbName = 'entries'
+mongoose.set('strictQuery', true)
 
 // ejs and static files
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(logger("dev"));
-app.use(methodOverride("_method"));
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(logger('dev'))
+app.use(methodOverride('_method'))
 // Sessions
 
-connectDB();
+connectDB()
 
 app.use(
-	session({
-		secret: [process.env.SESSION_SECRET, "cpd one two three"],
-		resave: false,
-		saveUninitialized: false,
-		store: MongoStore.create({
-			client: mongoose.connection.getClient(),
-			dbName: dbName,
-		}),
-	})
-);
+  session({
+    secret: [process.env.SESSION_SECRET, 'cpd one two three'],
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+      client: mongoose.connection.getClient(),
+      dbName: dbName
+    })
+  })
+)
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.use(flash());
+app.use(flash())
 
-app.use("/", mainRoutes);
-app.use("/entries", entryRoutes);
-app.use("/entries", mainRoutes);
+app.use('/', mainRoutes)
+app.use('/entries', entryRoutes)
+app.use('/entries', mainRoutes)
 
 app.listen(process.env.PORT || PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
