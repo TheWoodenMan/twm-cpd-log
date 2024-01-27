867061f75d20f0aac0227cb5372d26051f6220c0
Andy Wood <andynwood@gmail.com>
2024-01-27 13:17:14 +0000

    removed redundant mongoose options

--- config/database.js
+++ config/database.js
@@ -1,23 +1,15 @@
-const mongoose = require("mongoose");
-require("dotenv").config({ path: "./config/.env" });
-
-const mOptions = {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-};
+const mongoose = require('mongoose')
+require('dotenv').config({ path: './config/.env' })
 
 const connectDB = async () => {
-	try {
-		const conn = await mongoose.connect(process.env.DB_STRING, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+  try {
+    const conn = await mongoose.connect(process.env.DB_STRING)
 
-		console.log(`MongoDB Connected: ${conn.connection.host}`);
-	} catch (err) {
-		console.error(err);
-		process.exit(1);
-	}
-};
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (err) {
+    console.error(err)
+    process.exit(1)
+  }
+}
 
-module.exports = connectDB;
+module.exports = connectDB
